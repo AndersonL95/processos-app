10970a4d7e2d6d9233272c8a12362ac56f96808e
AndersonL95 <anderson-alves@hotmail.com>
2024-06-11 11:21:37 -0300

    login_screen design

---
+++ lib/src/application/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const customColors = {
+  'crismon': Color.fromRGBO(220, 20, 60, 1),
+  'green': Color.fromRGBO(1, 76, 45, 1),
+  'beige': Color.fromRGBO(255, 250, 240, 1),
+  'yellow': Color.fromRGBO(255, 255, 0, 1),
+  'white': Color.fromRGBO(255, 255, 255, 1),
+  'black': Color.fromRGBO(0, 0, 0, 1)
+};
--- lib/src/application/screens/login_page.dart
+++ lib/src/application/screens/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:processos_app/src/application/constants/colors.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -9,7 +10,83 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Container(
+      child: Scaffold(
+        backgroundColor: customColors["beige"],
+        body: SingleChildScrollView(
+          child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Image.asset(
+                "Assets/logo/DocInHand.png",
+                scale: 3.0,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
+              child: TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: customColors["green"],
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: customColors["green"]!),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(10))),
+                    labelText: "E-mail",
+                    hintText: "Digite o seu e-mail"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
+              child: TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.key,
+                      color: customColors["green"],
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: customColors["green"]!),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(10))),
+                    labelText: "Senha",
+                    hintText: "Digite a sua senha"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 70),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: customColors["green"],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    minimumSize: const Size(220, 55)),
+                onPressed: () {},
+                child: const Text(
+                  "Login",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+              ),
+            )
+          ]),
+        ),
+      ),
+    );
   }
 }
