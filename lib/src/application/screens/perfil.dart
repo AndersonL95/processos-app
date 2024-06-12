b25a047a8653bf8e445bf4319cd6ea5ee04a58de
AndersonL95 <anderson-alves@hotmail.com>
2024-06-11 23:21:36 -0300

    getUser info ok

--- lib/src/application/screens/perfil.dart
+++ lib/src/application/screens/perfil.dart
@@ -14,6 +14,8 @@ class _PerfilPageState extends State<PerfilPage> {
   final ApiService apiService = ApiService();
   late int id;
   Map<String, dynamic>? dataUser;
+  bool _loading = true;
+  String? _error;
 
   @override
   void initState() {
@@ -29,53 +31,67 @@ class _PerfilPageState extends State<PerfilPage> {
       final userData = await apiService.findUser(id);
       setState(() {
         dataUser = userData;
+        _loading = false;
       });
     } catch (e) {
       print(e);
+      _loading = false;
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Padding(
-          padding: EdgeInsets.only(top: 10),
-          child: Text(
-            "DocInHand",
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-          ),
-        ),
-        toolbarHeight: 120,
-        centerTitle: false,
-        backgroundColor: customColors['green'],
-        elevation: 0,
-        automaticallyImplyLeading: false,
-        actions: [
-          Padding(
+        appBar: AppBar(
+          title: Padding(
             padding: EdgeInsets.only(top: 10),
-            child: IconButton(
-              icon: Icon(
-                Icons.notification_important,
-                size: 30,
-                color: customColors['white'],
-              ),
-              onPressed: () {},
+            child: Text(
+              "DocInHand",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
           ),
-        ],
-      ),
-      body: Padding(
-        padding: EdgeInsets.only(top: 10, right: 10, left: 10),
-        child: Column(
-          children: [
-            /* Text(
-              "Nome: ${dataUser!['name']}",
-              style: TextStyle(fontSize: 24),
-            )*/
+          toolbarHeight: 120,
+          centerTitle: false,
+          backgroundColor: customColors['green'],
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          actions: [
+            Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: IconButton(
+                icon: Icon(
+                  Icons.notification_important,
+                  size: 30,
+                  color: customColors['white'],
+                ),
+                onPressed: () {},
+              ),
+            ),
           ],
         ),
-      ),
-    );
+        body: _loading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : _error != null
+                ? Center(
+                    child: Text("ERROR: $_error"),
+                  )
+                : dataUser != null
+                    ? Padding(
+                        padding: EdgeInsets.only(top: 20, left: 10, right: 10),
+                        child: Column(
+                          children: [
+                            Text(
+                              "Nome: ${dataUser!['name']}",
+                              style: TextStyle(fontSize: 24),
+                            )
+                          ],
+                        ),
+                      )
+                    : Center(
+                        child:
+                            Text("Não foi possivel carregas as informações."),
+                      ));
   }
 }
