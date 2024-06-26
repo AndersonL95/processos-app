d24924070bdb10a6082d8a97fd7fc81f15193138
AndersonL95 <anderson-alves@hotmail.com>
2024-06-25 22:20:30 -0300

    add_duplicate_not

--- lib/src/application/use-case/getUser_api.dart
+++ lib/src/application/use-case/getUser_api.dart
@@ -10,8 +10,6 @@ class GetUserInfoApi {
     final SharedPreferences data = await SharedPreferences.getInstance();
 
     try {
-      print("MEU ID: $id");
-
       var userData = await apiService.findUser(id);
       String userJson = jsonEncode(userData);
       await data.setString('userInfo', userJson);
